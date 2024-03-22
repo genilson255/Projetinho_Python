0aaae5c785fbd0818a8375d2883f873c900508a6
GENILSON MONTEIRO DA SILVA <57594800+genilson255@users.noreply.github.com>
2024-03-22 07:52:25 -0300

    Retoamdo o projeto python

--- src/Exec001.py
+++ src/Exec001.py
@@ -6,6 +6,9 @@ d = n1 / n2
 di = n1 // n2
 ex = n1 ** n2
 
-print("A soma é ({}), \n o produto é ({}) \n e a divisão é {:.3f}"
+print("A soma é ({}),"
+      "\n o produto é ({})"
+      "\n e a divisão é {:.3f}"
       .format(s, m, d))
-print('Divisao inteira ({}) e potencia {:.3f}  \n'.format(di, ex))
+print('Divisao inteira ({}) e potencia {:.3f}  '
+      '\n'.format(di, ex))
--- src/Exec002.py
+++ src/Exec002.py
@@ -1,7 +1,19 @@
 # Faça um Programa que leia um número inteiro
 # e mostre na tela o seu sucessor e seu antecessor
 
-n1 = int(input("Informe um numero: "))
+'''n1 = int(input("Informe um numero: "))
 sucess = n1 + 1
 antec = n1 - 1
 print("O numero informado foi {} seu antecessor é: {} seu sucesor é: {} ".format(n1, antec, sucess))
+'''
+
+# Para i na lista 234, 654, 378, 798:
+'''for i in [234, 654, 378, 798]:
+ # Se o resto dividindo por 3 for igual a zero:
+    if i % 3 == 0:
+        print(i, '/ 3 =', i / 3)'''
+
+s = 0
+for x in range(1, 100):
+    s = s + x
+    print( s)
